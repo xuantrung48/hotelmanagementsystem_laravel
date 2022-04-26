6b78801506d82c23d237a29c309a63f89232490c
nx_trung <nx_trung@brycen.com.vn>
2022-04-26 08:44:18 +0700

    add lang

---
+++ resources/lang/vi/errorMessages.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    "10000" => "Invalid Number!",
+    "10001" => "Start Date is greater than End Date!",
+    "10002" => "Result year is greater than current year!",
+    "10003" => "Company ID is not found!",
+    "10004" => "Data not found match with company id!",
+    "10005" => "File Not Found!",
+    "10006" => "Invalid CSV: File must have .csv extension.",
+    "10007" => "Can not divide by zero!",
+    "10008" => "Invalid Date according by this format :attribute",
+    "10009" => "Invalid Time according by this format :attribute",
+    "10010" => ":attribute is required!",
+    "10011" => "Invalid :attribute!",
+    "10012" => ":attribute does not exist in range.It is less than range!",
+    "10013" => ":attribute does not exist in range.It is greater than range!",
+
+    "20000" => "DB error ",
+    "20001" => "Nothing change because id and value exit ",
+
+    "SE001" => "Fail to Save Data!",
+    "SE002" => "Fail to Delete Data!",
+    "SE003" => "Fail to Update Data!",
+    "SE004" => ":attribute is Not Found!",
+    "SE005" => "This :attribute is already deleted!",
+    "SE006" => "This :attribute is already exist!",
+    "SE007" => ":attribute is already assigned in this role!.Please,choose other permission.",
+];
