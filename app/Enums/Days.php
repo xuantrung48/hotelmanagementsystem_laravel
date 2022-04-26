afe9e4a227dfbb79fc985a550b3c42918ef506b0
nx_trung <nx_trung@brycen.com.vn>
2022-04-26 13:23:29 +0700

    add Days enum

---
+++ app/Enums/Days.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static OptionOne()
+ * @method static static OptionTwo()
+ * @method static static OptionThree()
+ */
+final class Days extends Enum
+{
+    const Sunday = 0;
+    const Monday = 1;
+    const Tuesday = 2;
+    const Wednesday = 3;
+    const Thursday = 4;
+    const Friday = 5;
+    const Saturday = 6;
+}
